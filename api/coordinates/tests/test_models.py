7f3fc5fe4288e64316fbe20acb1882ef786fecd8
jeremyf2020 <jf@Asus.localdomain>
2025-12-22 04:31:39 +0000

    create coordinates folder, test folder, test_models

---
+++ api/coordinates/tests/test_models.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from api.coordinates.models import Coordinates
+
+class CoordinatesTest(TestCase):
+    # def test_coordinates(self):
+    #     """
+    #     Test that we can create a sector and the string representation is the name.
+    #     """
+    #     sector = Coordinates.objects.create(
+    #         name="RG1 1",
+    #         latitude=51.45,
+    #         longitude=-0.97,
+    #         population=500
+    #     )
+        
+    #     # 1. Verify Data Saved
+    #     self.assertEqual(Coordinates.objects.count(), 1)
+    #     self.assertEqual(sector.population, 500)
+        
+    #     # 2. Verify __str__ method (Crucial for Admin panel)
+    #     self.assertEqual(str(sector), "RG1 1")
+    pass
+
